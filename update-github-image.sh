c69a5d7862efeeeb094114dd443b2b11bc7ad324
pz <peterazink@gmail.com>
2025-07-27 09:13:30 -0400

    added ocean image for 2nd cam

--- update-github-image.sh
+++ update-github-image.sh
@@ -3,10 +3,12 @@ set -x
 
 #take photo
 imagesnap -d BRIO -w 1 surfcam-image.jpg
+imagesnap -d USB -w 1 ocean-image.jpg
 
 # Define variables
 REPO_PATH="/Users/pz/Library/Mobile Documents/com~apple~CloudDocs/Development/pzink.pithub.io Site/pzink.github.io/"
 IMAGE_PATH="/Users/pz/Library/Mobile Documents/com~apple~CloudDocs/Development/pzink.pithub.io Site/pzink.github.io/surfcam-image.jpg"
+IMAGE2_PATH="/Users/pz/Library/Mobile Documents/com~apple~CloudDocs/Development/pzink.pithub.io Site/pzink.github.io/ocean-image.jpg"
 COMMIT_MESSAGE="Automated image update"
 BRANCH_NAME="main" # Or your target branch
 
@@ -22,9 +24,11 @@ git config user.email "peterazink@gmail.com"
 
 # Copy or move the new image into the repository
 cp "$IMAGE_PATH" "./surfcam-image.jpg" # Example: Copy to an 'images' folder
+cp "$IMAGE2_PATH" "./ocean-image.jpg" # Example: Copy to an 'images' folder
 
 # Add the image file to staging
 git add "./surfcam-image.jpg"
+git add "./ocean-image.jpg"
 
 # Commit the changes
 git commit -m "$COMMIT_MESSAGE"
