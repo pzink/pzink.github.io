05778421d9cea77866be5ee762a1d869c248c68c
pz <peterazink@gmail.com>
2025-10-14 08:18:55 -0400

    update
    removed surfcam and Brio code; using only old webcam via USB

--- update-github-image.sh
+++ update-github-image.sh
@@ -6,7 +6,7 @@ export DISPLAY=:0
 
 # Define variables for each cam
 REPO_PATH="/Users/pz/Library/Mobile Documents/com~apple~CloudDocs/Development/Github site/pzink.github.io/"
-IMAGE_PATH="./surfcam-image.jpg"
+#IMAGE_PATH="./surfcam-image.jpg"
 IMAGE2_PATH="./ocean-image.jpg"
 COMMIT_MESSAGE="Automated image update"
 BRANCH_NAME="main" # Or your target branch
@@ -17,7 +17,7 @@ cd "$REPO_PATH"
 echo $PATH
 
 #take photo
-/usr/local/bin/imagesnap -d BRIO -w 0 surfcam-image.jpg 2>&1 >> ./imagesnap_cron.log
+#/usr/local/bin/imagesnap -d BRIO -w 0 surfcam-image.jpg 2>&1 >> ./imagesnap_cron.log
 /usr/local/bin/imagesnap -d USB -w 0 ocean-image.jpg
 
 sips ocean-image.jpg -o ocean-image.jpg --cropOffset 250 0 -c 350 1280
@@ -30,15 +30,15 @@ git config user.name "pz"
 git config user.email "peterazink@gmail.com"
 
 # Copy or move the new image into the repository
-cp "$IMAGE_PATH" "images/surfcam-image.jpg" 
+#cp "$IMAGE_PATH" "images/surfcam-image.jpg" 
 cp "$IMAGE2_PATH" "images/ocean-image.jpg"
 
 # Add the image file to staging
-git add "./surfcam-image.jpg"
+#git add "./surfcam-image.jpg"
 git add "./ocean-image.jpg"
 
 # remove images
-rm "./surfcam-image.jpg" 
+#rm "./surfcam-image.jpg" 
 rm "./ocean-image.jpg"
 
 # Commit the changes
