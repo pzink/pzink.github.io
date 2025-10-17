0457a6b015b319800dfeed7f8df893d653e40af2
pz <peterazink@gmail.com>
2025-10-17 09:53:13 -0400

    updated script

--- update-github-image.sh
+++ update-github-image.sh
@@ -24,32 +24,19 @@ sips surfcam-image.jpg -o surfcam-image.jpg --cropOffset 150 0 -c 300 1280
 sips ocean-image.jpg -o ocean-image.jpg --cropOffset 900 0 -c 1600 4416
 
 # add date time stamp to images
-let ps=$(identify -format '%w' surfcam-image.jpg)/20
-# 2023:10:04 09:29:24
-datetime=$(identify -format  %[exif:datetime] surfcam-image.jpg)
-if [ -z "$datetime" ]
-then    # date:create: 2023-11-11T14:40:21+00:00
-        datetime=$(identify -format  %[date:create] surfcam-image.jpg)
-fi
-datetime=$(echo "$datetime" | gawk -F'[- :T+]' '{print $3"-"$2"-"$1 " " $4":"$5}' )
-magick surfcam-image.jpg -fill black  -undercolor white -pointsize "$ps" -gravity southeast \
- -annotate 0 "$datetime" surfcam-image.jpg
+let ps=$(identify -format '%w' surfcam-image.jpg)/20 # get width
+datetime=$(identify -format "%[date:create]" surfcam-image.jpg)
+$datetime=$(echo "$datetime" | gawk -F'[- :T+]' '{print $3"-"$2"-"$1 " " $4":"$5}' )
+magick surfcam-image.jpg -fill black  -undercolor white -pointsize "$ps" -gravity southeast -annotate 0 "$datetime" surfcam-image.jpg
 
 let ps=$(identify -format '%w' ocean-image.jpg)/20
-# 2023:10:04 09:29:24
-datetime=$(identify -format  %[exif:datetime] ocean-image.jpg)
-if [ -z "$datetime" ]
-then    # date:create: 2023-11-11T14:40:21+00:00
-        datetime=$(identify -format  %[date:create] ocean-image.jpg)
-fi
-datetime=$(echo "$datetime" | gawk -F'[- :T+]' '{print $3"-"$2"-"$1 " " $4":"$5}' )
-magick ocean-image.jpg -fill black  -undercolor white -pointsize "$ps" -gravity southeast \
- -annotate 0 "$datetime" ocean-image.jpg
+datetime=$(identify -format "%[date:create]" ocean-image.jpg)
+magick ocean-image.jpg -fill black  -undercolor white -pointsize "$ps" -gravity southeast -annotate 0 "$datetime" ocean-image.jpg
 
 #return to main git dir
 cd ..
 
-# Ensure the remote uses SSH (replace with your actual repo)
+# Ensure the remote uses SSH 
 git remote set-url origin git@github.com:pzink/pzink.github.io.git
 
 # Set Git user name and email
@@ -58,7 +45,7 @@ git config user.email "peterazink@gmail.com"
 
 git pull --rebase
 
-# Copy or move the new image into the repository
+# Copy or move the new images into the repository
 git add "images/ocean-image.jpg"
 git add "images/surfcam-image.jpg"
 
