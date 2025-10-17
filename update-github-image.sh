c39336cb72c07c1e0228eff89fd63a16657f7a1d
pz <peterazink@gmail.com>
2025-10-17 09:26:36 -0400

    updated script

--- update-github-image.sh
+++ update-github-image.sh
@@ -14,14 +14,39 @@ BRANCH_NAME="main" # Or your target branch
 # Navigate to the repository
 cd "$REPO_PATH"
 
+# take image with the web cam
 imagesnap -d USB -w 1 surfcam-image.jpg 
 mv surfcam-image.jpg images/surfcam-image.jpg
-
 
 #Crop images
 cd images
 sips surfcam-image.jpg -o surfcam-image.jpg --cropOffset 150 0 -c 300 1280
 sips ocean-image.jpg -o ocean-image.jpg --cropOffset 900 0 -c 1600 4416
+
+# add date time stamp to images
+let ps=$(identify -format '%w' surfcam-image.jpg)/20
+# 2023:10:04 09:29:24
+datetime=$(identify -format  %[exif:datetime] surfcam-image.jpg)
+if [ -z "$datetime" ]
+then    # date:create: 2023-11-11T14:40:21+00:00
+        datetime=$(identify -format  %[date:create] surfcam-image.jpg)
+fi
+datetime=$(echo "$datetime" | gawk -F'[- :T+]' '{print $3"-"$2"-"$1 " " $4":"$5}' )
+magick surfcam-image.jpg -fill black  -undercolor white -pointsize "$ps" -gravity southeast \
+ -annotate 0 "$datetime" surfcam-image.jpg
+
+let ps=$(identify -format '%w' ocean-image.jpg)/20
+# 2023:10:04 09:29:24
+datetime=$(identify -format  %[exif:datetime] ocean-image.jpg)
+if [ -z "$datetime" ]
+then    # date:create: 2023-11-11T14:40:21+00:00
+        datetime=$(identify -format  %[date:create] ocean-image.jpg)
+fi
+datetime=$(echo "$datetime" | gawk -F'[- :T+]' '{print $3"-"$2"-"$1 " " $4":"$5}' )
+magick ocean-image.jpg -fill black  -undercolor white -pointsize "$ps" -gravity southeast \
+ -annotate 0 "$datetime" ocean-image.jpg
+
+#return to main git dir
 cd ..
 
 # Ensure the remote uses SSH (replace with your actual repo)
